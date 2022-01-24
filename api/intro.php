e51a9a017eb1d49fb856c89a00c0045bbee4a152
Dicas & Tutoriais <42816631+Dicasetutoriais@users.noreply.github.com>
2022-01-24 13:30:44 +0000

    Delete api directory

--- api/intro.php
+++
@@ -1,4 +0,0 @@
-<?php 
-print "<head><meta name=\"viewport\" content=\"video/mp4\"></head>\n";
-print "<body><video controls=\"\" autoplay=\"\" name=\"media\"><source src=\"../intro.mp4\" type=\"video/mp4\"></video></body>";
-
